3f79a2817e96f9bde794d3ba8fb747438a5cd6c2
yogabagus251 <137670939+yogabagus251@users.noreply.github.com>
2023-06-25 12:14:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var navlink = document.getElementById("navlink");{
+ function showMenu(){
+     navlink.style.right = "0";
+    }
+ function hideMenu(){
+     navlink.style.right = "-200px";
+    }  
+      
+};
+
+const span = document.getElementsByTagName('span');
+ span.addEventListener('click',function(){
+    span.style.font-size =="50px";
+
+ });
+
+
+
+
